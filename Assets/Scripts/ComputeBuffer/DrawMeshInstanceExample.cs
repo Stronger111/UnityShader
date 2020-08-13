adc487a142b6e535871acf55662c0fd9c54c7712
776239125@qq.com <776239125@qq.com>
2020-08-13 15:35:14 +0800

    【DrawMeshInstancedIndirect】

---
+++ Assets/Scripts/ComputeBuffer/DrawMeshInstanceExample.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawMeshInstanceExample : MonoBehaviour
+{
+    public int instanceCount = 100000;
+    public Mesh instanceMesh;
+    public Material instanceMaterial;
+    public int subMeshIndex = 0;
+
+    private int cachedInstanceCount = -1;
+    private int cachedSubMeshIndex = -1;
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5] { 0,0,0,0,0};
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        UpdateBuffers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update starting position buffer
+        if (cachedInstanceCount != instanceCount || cachedSubMeshIndex != subMeshIndex)
+            UpdateBuffers();
+        // Pad input
+        if (Input.GetAxisRaw("Horizontal") != 0.0f)
+            instanceCount = (int)Mathf.Clamp(instanceCount+Input.GetAxis("Horizontal")*40000,1.0f,5000000.0f);
+        // Render
+        Graphics.DrawMeshInstancedIndirect(instanceMesh,subMeshIndex,instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)),argsBuffer);
+    }
+
+    void UpdateBuffers()
+    {
+        // Ensure submesh index is in range
+        if (instanceMesh != null)
+            subMeshIndex = Mathf.Clamp(subMeshIndex,0,instanceMesh.subMeshCount-1);
+        // Positions
+        if (positionBuffer != null)
+            positionBuffer.Release();
+        positionBuffer = new ComputeBuffer(instanceCount,16);//4个float
+        Vector4[] positions = new Vector4[instanceCount];
+        for(int InstanceIndex=0; InstanceIndex<instanceCount; InstanceIndex++)
+        {
+            float angle = Random.Range(0.0f,Mathf.PI*2.0f);
+            float distance = Random.Range(20.0f,100.0f);
+            float height = Random.Range(-2.0f,2.0f);
+            float size = Random.Range(0.05f,0.25f);
+            positions[InstanceIndex] = new Vector4(Mathf.Sin(angle)*distance,height,Mathf.Cos(angle)*distance,size);  //每个分量代表的含义不同
+        }
+        positionBuffer.SetData(positions);
+        instanceMaterial.SetBuffer("positionBuffer", positionBuffer);
+        // Indirect args
+        if(instanceMesh!=null)
+        {
+            args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
+            args[1] = (uint)instanceCount;
+            args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
+            args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
+        }
+        else
+        {
+            args[0] = args[1] = args[2] = args[3] = 0;
+        }
+        argsBuffer.SetData(args);
+        cachedInstanceCount = instanceCount;
+        cachedSubMeshIndex = subMeshIndex;
+    }
+
+    private void OnDisable()
+    {
+        if (positionBuffer != null)
+            positionBuffer.Release();
+        positionBuffer = null;
+
+        if (argsBuffer != null)
+            argsBuffer.Release();
+        argsBuffer = null;
+    }
+}
