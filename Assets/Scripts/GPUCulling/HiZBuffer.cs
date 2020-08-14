4e5a418a39d8d75e28eeb25531d2cbd425ab10f4
776239125@qq.com <776239125@qq.com>
2020-08-14 16:51:26 +0800

    【Draw Mesh Hiz Cull Hiz Command Buffer】

---
+++ Assets/Scripts/GPUCulling/HiZBuffer.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Camera))]
+public class HiZBuffer : MonoBehaviour
+{
+    #region Variables
+    [Header("Reference")]
+    //todo 这张RT作用
+    public RenderTexture topDownView = null;
+    public IndirectRenderer m_IndirectRenderer;
+    public Camera mainCamera = null;
+    public Light light = null;
+    //Shader ?
+    public Shader generateBufferShader = null;
+    public Shader debugShader = null;
+
+    // private
+    private int m_LODCount = 0;
+    private int[] m_Temporaries = null;
+    //反射之后？
+    private CameraEvent m_CameraEvent = CameraEvent.AfterReflections;
+    /// <summary>
+    /// 纹理大小
+    /// </summary>
+    private Vector2 m_textureSize;
+    private Material m_generateBufferMaterial = null;
+    private Material m_debugMaterial = null;
+    /// <summary>
+    /// HZB 深度图
+    /// </summary>
+    private RenderTexture m_HiZDepthTexture = null;
+    private CommandBuffer m_CommandBuffer = null;
+    private CameraEvent m_lastCameraEvent = CameraEvent.AfterReflections;
+    private RenderTexture m_ShadowmapCopy = null;
+    private RenderTargetIdentifier m_Shadowmap;
+    private CommandBuffer m_lightShadowCommandBuffer;
+
+    public Vector2 TextureSize { get { return m_textureSize; } }
+    public RenderTexture Texture { get { return m_HiZDepthTexture; } }
+
+    private const int MAXIMUM_BUFFER_SIZE = 1024;
+
+    private enum Pass
+    {
+        Blit,
+        Reduce
+    }
+    #endregion
+    private void Awake()
+    {
+        m_generateBufferMaterial = new Material(generateBufferShader);
+        m_debugMaterial = new Material(debugShader);
+        mainCamera.depthTextureMode = DepthTextureMode.Depth;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        if (mainCamera != null)
+        {
+            if (m_CommandBuffer != null)
+            {
+                mainCamera.RemoveCommandBuffer(m_CameraEvent, m_CommandBuffer);
+                m_CommandBuffer = null;
+            }
+        }
+        if (m_HiZDepthTexture != null)
+        {
+            m_HiZDepthTexture.Release();
+            m_HiZDepthTexture = null;
+        }
+    }
+
+    public void InitializeTexture()
+    {
+        if (m_HiZDepthTexture != null)
+            m_HiZDepthTexture.Release();
+
+        int size = Mathf.Max(mainCamera.pixelWidth, mainCamera.pixelHeight);
+        size = Mathf.Min(Mathf.NextPowerOfTwo(size), MAXIMUM_BUFFER_SIZE);
+        m_textureSize.x = size;
+        m_textureSize.y = size;
+        //深度是线性 的 
+        m_HiZDepthTexture = new RenderTexture(size, size, 0, RenderTextureFormat.RGHalf, RenderTextureReadWrite.Linear);
+        m_HiZDepthTexture.filterMode = FilterMode.Point;
+        m_HiZDepthTexture.useMipMap = true;
+        m_HiZDepthTexture.autoGenerateMips = false;
+        bool isCreate = m_HiZDepthTexture.Create();
+        if (isCreate)
+            m_HiZDepthTexture.hideFlags = HideFlags.HideAndDontSave;
+        else
+            Debug.LogError(string.Format("创建m_HiZDepthTexture失败{0}", 33));
+    }
+    /// <summary>
+    /// 在渲染场景之前进行调用
+    /// </summary>
+    private void OnPreRender()
+    {
+        int size = Mathf.Max(mainCamera.pixelWidth, mainCamera.pixelHeight);
+        size = Mathf.Min(Mathf.NextPowerOfTwo(size), MAXIMUM_BUFFER_SIZE);
+        m_textureSize.x = size;
+        m_textureSize.y = size;
+
+        m_LODCount = Mathf.FloorToInt(Mathf.Log(size, 2f));
+
+        if (m_LODCount == 0)
+            return;
+        else
+            Debug.Log(string.Format("LOD Count-----{0}", m_LODCount));
+
+        bool isCommandBufferInvalid = false;
+        if (m_HiZDepthTexture == null || (m_HiZDepthTexture.width != size || m_HiZDepthTexture.height != size) || m_lastCameraEvent != m_CameraEvent)
+        {
+            InitializeTexture();
+            m_lastCameraEvent = m_CameraEvent;
+            isCommandBufferInvalid = true;
+        }
+
+        if (m_CommandBuffer == null || isCommandBufferInvalid)
+        {
+            m_Temporaries = new int[m_LODCount];
+
+            if (m_CommandBuffer != null)
+                mainCamera.RemoveCommandBuffer(m_CameraEvent, m_CommandBuffer);
+
+            m_CommandBuffer = new CommandBuffer();
+            m_CommandBuffer.name = "Hi-Z Buffer";
+
+            RenderTargetIdentifier RenderTargetID = new RenderTargetIdentifier(m_HiZDepthTexture);
+            m_CommandBuffer.SetGlobalTexture("_LightTexture", m_ShadowmapCopy);
+            m_CommandBuffer.Blit(null, RenderTargetID, m_generateBufferMaterial, (int)Pass.Blit);
+
+            for (int LODIndex = 0; LODIndex < m_LODCount; LODIndex++)
+            {
+                m_Temporaries[LODIndex] = Shader.PropertyToID("_0965d57_Temporaries" + LODIndex.ToString());
+                //除 2操作
+                size >>= 1;
+                size = Mathf.Max(size, 1);
+                //
+                m_CommandBuffer.GetTemporaryRT(m_Temporaries[LODIndex], size, size, 0, FilterMode.Point, RenderTextureFormat.RGHalf, RenderTextureReadWrite.Linear);
+
+                if (LODIndex == 0)
+                    m_CommandBuffer.Blit(RenderTargetID, m_Temporaries[0], m_generateBufferMaterial, (int)Pass.Reduce);
+                else
+                    m_CommandBuffer.Blit(m_Temporaries[LODIndex - 1], m_Temporaries[LODIndex], m_generateBufferMaterial, (int)Pass.Reduce);
+                //到最后是RenderTargetID 是一张降采样的图
+                m_CommandBuffer.CopyTexture(m_Temporaries[LODIndex], 0, 0, RenderTargetID, 0, LODIndex + 1);
+
+                if (LODIndex >= 1)
+                    m_CommandBuffer.ReleaseTemporaryRT(m_Temporaries[LODIndex - 1]);
+            }
+
+            m_CommandBuffer.ReleaseTemporaryRT(m_Temporaries[m_LODCount - 1]);
+            mainCamera.AddCommandBuffer(m_CameraEvent, m_CommandBuffer);
+        }
+    }
+
+    /// <summary>
+    /// 在渲染完所有场景后进行调用
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //Debug
+        //if()
+        //{
+
+        //}
+        Graphics.Blit(source,destination);
+    }
+}
---
+++ Assets/Scripts/GPUCulling/IndirectRenderer.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// Preferrably want to have all buffer structs in power of 2...
+// 6 * 4 bytes = 24 bytes  //6个 4个bytes
+[System.Serializable]
+[StructLayout(LayoutKind.Sequential)] //内存布局顺序布局方式
+public struct IndirectInstanceCSInput
+{
+    public Vector3 boundsCenter;  //3  +12
+    public Vector3 boundsExtents;  //6 +12
+}
+
+// 8 * 4 bytes = 32 bytes
+[StructLayout(LayoutKind.Sequential)]
+public struct Indirect2x2Matrix
+{
+    public Vector4 row0;
+    public Vector4 row1;  //16+16=32
+}
+
+// 2 * 4 bytes = 8 bytes
+public class SortingData
+{
+    public uint drawCallInstanceIndex;  //1
+    public float distanceToCam;  //2
+}
+
+[System.Serializable]
+public class IndirectRenderingMesh
+{
+    public Mesh mesh;
+    public Material material;
+    //Lod Shader
+    public MaterialPropertyBlock lod00MatPropBlock;
+    public MaterialPropertyBlock lod01MatPropBlock;
+    public MaterialPropertyBlock lod02MatPropBlock;
+    //LOD Shadow
+    public MaterialPropertyBlock shadowLod00MatPropBlock;
+    public MaterialPropertyBlock shadowLod01MatPropBlock;
+    public MaterialPropertyBlock shadowLod02MatPropBlock;
+    //Vertices
+    public uint numOfVerticesLod00;
+    public uint numOfVerticesLod01;
+    public uint numOfVerticesLod02;
+    //Indices
+    public uint numOfIndicesLod00;
+    public uint numOfIndicesLod01;
+    public uint numOfIndicesLod02;
+}
+public class IndirectRenderer : MonoBehaviour
+{
+    #region 变量
+    [Header("设置")]
+    public bool runCompute = true;
+    public bool drawInstance = true;
+    public bool enableInstanceShadows = true;
+    public bool enableOcclusionCulling = true;
+    public bool enableDetailCulling = true;
+    public bool enableLOD = true;
+    public bool enableOnlyLOD02Shadows = true;
+    [Range(00.00f, 00.02f)] public float detailCullingPercentage = 0.005f;  //百分比
+
+    //Debug 变量
+    [Header("Debug")]
+    public bool debugShowUI;
+    public bool debugDrawLOD;
+    public bool debugDrawBoundsInSceneView;
+    public bool debugDrawHiZ;
+    [Range(0, 10)] public int debugHiZLOD;
+    public GameObject debugUIPrefab;
+
+    [Header("Data")]
+    [ReadOnly] public List<IndirectInstanceCSInput> instancesInputData = new List<IndirectInstanceCSInput>();
+    [ReadOnly] public IndirectRenderingMesh[] indirectMeshes;
+
+    [Header("Logging")]
+    public bool logInstanceDrawMatrices = false;
+    public bool logArgumentsAfterReset = false;
+    public bool logSortingData = false;
+    public bool logInstancesIsVisibleBuffer = false;
+    public bool logScannedPreicates = false;
+    public bool logGroupSumArrayBuffer = false;
+    public bool logScannedGroupSumsBuffer = false;
+    public bool logArgsBufferAfterCopy = false;
+    public bool logCulledInstancesDrawMatrices = false;
+
+    [Header("References")]
+    public ComputeShader createDrawDataBufferCS;
+    public ComputeShader sortingCS;
+    public ComputeShader occlusionCS;
+    public ComputeShader scanInstancesCS;
+    public ComputeShader scanGroupSumsCS;
+    public ComputeShader copyInstanceDataCS;
+    public HiZBuffer hiZBuffer;
+    public Camera mainCamera;
+    public Camera debugCamera;
+
+    //Compute Buffers
+    private ComputeBuffer m_instancesIsVisibleBuffer;
+    private ComputeBuffer m_instancesGroupSumArrayBuffer;
+    //Scan
+    private ComputeBuffer m_instancesScannedGroupSumBuffer;
+    private ComputeBuffer m_instancesScannedPreicates;
+    private ComputeBuffer m_instanceDataBuffer;
+    //Sort Compute Buffer
+    private ComputeBuffer m_instancesSortingData;
+    private ComputeBuffer m_instancesSortingDataTemp;
+    //矩阵
+    private ComputeBuffer m_instancesMatrixRows01;
+    private ComputeBuffer m_instancesMatrixRows23;
+    private ComputeBuffer m_instancesMatrixRows45;
+    //Cull 矩阵
+    private ComputeBuffer m_instancesCulledMatrixRow01;
+    private ComputeBuffer m_instancesCulledMatrixRows23;
+    private ComputeBuffer m_instancesCulledMatrixRows45;
+    //Args Buffer
+    private ComputeBuffer m_instancesArgsBuffer;
+    //Shadow Args
+    private ComputeBuffer m_shadowArgsBuffer;
+    private ComputeBuffer m_shadowsIsVisibleBuffer;
+    //分组 
+    private ComputeBuffer m_shadowGroupSumArrayBuffer;
+    private ComputeBuffer m_shadowsScannedGroupSumBuffer;
+    //Predicates 判断
+    private ComputeBuffer m_shadowScannedInstancePredicates;
+    //shadow Culled
+    private ComputeBuffer m_shadowCulledMatrixRows01;
+    private ComputeBuffer m_shadowCulledMatrixRows23;
+    private ComputeBuffer m_shadowCulledMatrixRows45;
+
+    //Command Buffer
+    private CommandBuffer m_sortingCommandBuffer;
+
+    //Kernel ID s  Compute Shader
+    private int m_createDrawDataBufferKernelID;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
