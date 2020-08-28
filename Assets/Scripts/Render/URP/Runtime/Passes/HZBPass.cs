ca120eb61a41b82fba2e92609fb09906bf685e9a
776239125@qq.com <776239125@qq.com>
2020-08-28 09:17:33 +0800

    HZB

---
+++ Assets/Scripts/Render/URP/Runtime/Passes/HZBPass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.Rendering.Universal.Internal
+{
+    /// <summary>
+    /// Hi z Buffer Pass 用深度金字塔 上一帧深度做查询进行可见性
+    /// 物体的显示和隐藏 version:SetActive 
+    /// </summary>
+    public class HZBPass : ScriptableRenderPass
+    {
+        const string m_ProfilerName = "Hi Z Buffer Pass";
+        ProfilingSampler m_ProfilingSampler;
+        public HZBPass()
+        {
+            m_ProfilingSampler = new ProfilingSampler(m_ProfilerName);
+        }
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerName);
+
+            using (new ProfilingScope(cmd, m_ProfilingSampler))
+            {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+            }
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
+
---
+++ Assets/Version.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Version : MonoBehaviour
+{
+    /****************    版本说明   记录 一下 ***************************/
+    //1: 冯乐乐 Shader 代码
+    //2: 接入Unity 的URP 版本 7.3.1 Unity版本2019.4.8
+       //2.1:UE HZB Pass根据层级深度剔除物体
+}
